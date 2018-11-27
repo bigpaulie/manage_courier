81e1af7914e879e93f289474173d3178a0197b7d
Abhishek Jain <abhishek.jain@plenartech.com>
2018-11-27 16:32:30 +0530

    hide notification count form header..

--- resources/views/_includes/admin/header.blade.php
+++ resources/views/_includes/admin/header.blade.php
@@ -20,9 +20,9 @@
             {{--</div>--}}
         {{--</form>--}}
 
-        <span class="separator"></span>
+        {{--<span class="separator"></span>--}}
 
-        <ul class="notifications">
+        {{--<ul class="notifications">--}}
             {{--<li>--}}
                 {{--<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">--}}
                     {{--<i class="fa fa-tasks"></i>--}}
@@ -130,58 +130,58 @@
                     {{--</div>--}}
                 {{--</div>--}}
             {{--</li>--}}
-            <li>
-                <a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-                    <i class="fa fa-bell"></i>
-                    <span class="badge">3</span>
-                </a>
+            {{--<li>--}}
+                {{--<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">--}}
+                    {{--<i class="fa fa-bell"></i>--}}
+                    {{--<span class="badge">3</span>--}}
+                {{--</a>--}}
 
-                <div class="dropdown-menu notification-menu">
-                    <div class="notification-title">
-                        <span class="pull-right label label-default">3</span>
-                        Notifications
-                    </div>
+                {{--<div class="dropdown-menu notification-menu">--}}
+                    {{--<div class="notification-title">--}}
+                        {{--<span class="pull-right label label-default">3</span>--}}
+                        {{--Notifications--}}
+                    {{--</div>--}}
 
-                    <div class="content">
-                        <ul>
-                            <li>
-                                <a href="#" class="clearfix">
-                                    <div class="image">
-                                        <i class="fa fa-thumbs-down bg-danger"></i>
-                                    </div>
-                                    <span class="title">Server is Down!</span>
-                                    <span class="message">Just now</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="clearfix">
-                                    <div class="image">
-                                        <i class="fa fa-lock bg-warning"></i>
-                                    </div>
-                                    <span class="title">User Locked</span>
-                                    <span class="message">15 minutes ago</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="clearfix">
-                                    <div class="image">
-                                        <i class="fa fa-signal bg-success"></i>
-                                    </div>
-                                    <span class="title">Connection Restaured</span>
-                                    <span class="message">10/10/2014</span>
-                                </a>
-                            </li>
-                        </ul>
+                    {{--<div class="content">--}}
+                        {{--<ul>--}}
+                            {{--<li>--}}
+                                {{--<a href="#" class="clearfix">--}}
+                                    {{--<div class="image">--}}
+                                        {{--<i class="fa fa-thumbs-down bg-danger"></i>--}}
+                                    {{--</div>--}}
+                                    {{--<span class="title">Server is Down!</span>--}}
+                                    {{--<span class="message">Just now</span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="#" class="clearfix">--}}
+                                    {{--<div class="image">--}}
+                                        {{--<i class="fa fa-lock bg-warning"></i>--}}
+                                    {{--</div>--}}
+                                    {{--<span class="title">User Locked</span>--}}
+                                    {{--<span class="message">15 minutes ago</span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                            {{--<li>--}}
+                                {{--<a href="#" class="clearfix">--}}
+                                    {{--<div class="image">--}}
+                                        {{--<i class="fa fa-signal bg-success"></i>--}}
+                                    {{--</div>--}}
+                                    {{--<span class="title">Connection Restaured</span>--}}
+                                    {{--<span class="message">10/10/2014</span>--}}
+                                {{--</a>--}}
+                            {{--</li>--}}
+                        {{--</ul>--}}
 
-                        <hr />
+                        {{--<hr />--}}
 
-                        <div class="text-right">
-                            <a href="#" class="view-more">View All</a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
+                        {{--<div class="text-right">--}}
+                            {{--<a href="#" class="view-more">View All</a>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+        {{--</ul>--}}
 
         <span class="separator"></span>
 
