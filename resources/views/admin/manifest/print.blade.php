b28f07c97d5f72f41c3da979d1419b35ce669780
Abhishek Jain <abhishek.jain@plenartech.com>
2018-12-28 19:20:06 +0530

    removed and added source and destination address

--- resources/views/admin/manifest/print.blade.php
+++ resources/views/admin/manifest/print.blade.php
@@ -62,8 +62,8 @@
                                 <th id="cell-item" class="text-semibold">Recipient Name</th>
                                 <th id="cell-desc" class="text-semibold">Source</th>
                                 <th id="cell-price" class="text-center text-semibold">Destination</th>
-                                <th id="cell-qty" class="text-center text-semibold">No. of Boxes</th>
-                                <th id="cell-total" class="text-center text-semibold">Weight(KGS)</th>
+                                {{--<th id="cell-qty" class="text-center text-semibold">No. of Boxes</th>--}}
+                                {{--<th id="cell-total" class="text-center text-semibold">Weight(KGS)</th>--}}
                                 <th id="cell-total" class="text-center text-semibold">Sign</th>
 
 
@@ -74,12 +74,34 @@
 
                                 <tr>
                                     <td>{{$item_courier->unique_name}}</td>
-                                    <td class="text-semibold text-dark">{{$item_courier->s_name}} ({{$item_courier->s_company}})</td>
-                                    <td class="text-semibold text-dark">{{$item_courier->r_name}} ({{$item_courier->r_company}})</td>
-                                    <td>{{$item_courier->s_state}}, {{$item_courier->sender_country->name}}</td>
-                                    <td class="text-center">{{$item_courier->r_state}}, {{$item_courier->receiver_country->name}}</td>
-                                    <td class="text-center">{{$item_courier->no_of_boxes}}</td>
-                                    <td class="text-center">@if(isset($item_courier->shippment)){{$item_courier->shippment->weight}}@endif</td>
+                                    <td class="text-semibold text-dark">
+
+                                        {{$item_courier->s_name}} ({{$item_courier->s_company}})
+                                    </td>
+                                    <td class="text-semibold text-dark">
+                                        {{$item_courier->r_name}} ({{$item_courier->r_company}})</td>
+                                    <td class="text-capitalize">
+                                        {{$item_courier->s_address1}}
+                                        <br>
+                                        {{$item_courier->s_city}}, {{$item_courier->s_state}}
+                                        <br>
+                                        {{$item_courier->sender_country->name}}
+                                        <br>
+                                        Ph: {{$item_courier->s_phone}}
+                                        </td>
+                                    <td class="text-center text-capitalize">
+
+                                        {{$item_courier->r_address1}}
+                                        <br>
+                                        {{$item_courier->r_city}}, {{$item_courier->r_state}}
+                                        <br>
+                                        {{$item_courier->receiver_country->name}}, {{$item_courier->r_zip_code}}
+                                        <br>
+                                        Ph: {{$item_courier->r_phone}}
+
+                                    </td>
+                                    {{--<td class="text-center">{{$item_courier->no_of_boxes}}</td>--}}
+                                    {{--<td class="text-center">@if(isset($item_courier->shippment)){{$item_courier->shippment->weight}}@endif</td>--}}
                                     <td></td>
                                 </tr>
                             @endforeach
